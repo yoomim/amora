39d983eef519c44d9a211f44ecd9d9eb015cf800
yoomim <162602934+yoomim@users.noreply.github.com>
2024-05-17 11:41:47 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -16,9 +16,9 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-let tempoAtual = new Date();
+let tempoAtual = new Date("2024-10-05");
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+contadores[0].textContent =tempoObjetivo1;
 
 
 
